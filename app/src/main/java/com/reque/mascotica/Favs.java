88c4d8c0ea7787bc4fd5796b0680ac615fa21dd5
erick98 <ericksalazar_g@hotmail.com>
2019-04-07 19:35:34 -0600

    clases de Favs y User hechas con constructores y setter&getters

--- app/src/main/java/com/reque/mascotica/Favs.java
+++ app/src/main/java/com/reque/mascotica/Favs.java
@@ -1,4 +1,27 @@
 package com.reque.mascotica;
 
 public class Favs {
+    private int idFav;
+    private int idPublicacion;
+
+    public Favs(int idFav, int idPublicacion) {
+        this.idFav = idFav;
+        this.idPublicacion = idPublicacion;
+    }
+
+    public int getIdFav() {
+        return idFav;
+    }
+
+    public void setIdFav(int idFav) {
+        this.idFav = idFav;
+    }
+
+    public int getIdPublicacion() {
+        return idPublicacion;
+    }
+
+    public void setIdPublicacion(int idPublicacion) {
+        this.idPublicacion = idPublicacion;
+    }
 }
--- app/src/main/java/com/reque/mascotica/User.java
+++ app/src/main/java/com/reque/mascotica/User.java
@@ -1,4 +1,88 @@
 package com.reque.mascotica;
 
 public class User {
+    private String email;
+    private String name;
+    private String lastname;
+    private String password;
+    private String phone;
+    private String profiledesc;
+    private int id;
+    private boolean organization;
+
+    public User(String email, String name, String lastname, String password, String phone, String profiledesc, int id, boolean organization) {
+        this.email = email;
+        this.name = name;
+        this.lastname = lastname;
+        this.password = password;
+        this.phone = phone;
+        this.profiledesc = profiledesc;
+        this.id = id;
+        this.organization = organization;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getProfiledesc() {
+        return profiledesc;
+    }
+
+    public void setProfiledesc(String profiledesc) {
+        this.profiledesc = profiledesc;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(boolean organization) {
+        this.organization = organization;
+    }
+
 }
